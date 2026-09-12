96fa2d42a2315d7da104c0759b484335fcef4aad
Feng Gao <peak.gao.engineer@gmail.com>
2026-09-13 01:36:28 +0800

    post: 个人财务自由

--- data/posts.js
+++ data/posts.js
@@ -2,4 +2,14 @@
  * 文章数据源（纯静态，无后端）
  * 每篇文章：{ id, title, tags[], date, content(富文本 HTML) }
  */
-window.__POSTS__ = [];
+window.__POSTS__ = [
+  {
+    "id": "20260912-36cst",
+    "title": "个人财务自由",
+    "tags": [
+      "财务自由"
+    ],
+    "date": "2026-09-12",
+    "content": "<div><span style=\"background-color: rgb(242, 243, 245); color: rgb(119, 124, 133); font-size: 12px;\">图片上传中…</span><img src=\"images/img-20260912-3fkq3.jpg\" alt=\"\">所谓的个人财富自由，指的是某个人再也不用为了满足生活必需而出售自己的时间了。够简洁、准确、正确，而且有指导意义。</div><div><br></div><div>在你获得财富自由之后，还是会不断做的事情，比如，专注成长而不是专注成功，很多人沒有意识到的是，不管你获得</div><div>了怎样的成功，你依然需要成长，财富自由不是终点，那只是通往终点过程中的一个里程碑而已。</div><div><br></div>"
+  }
+];
